ba37f6bbdab282e85d000933a178fc1c8e9fea4a
kamil <frankiewiczkamil@gmail.com>
2022-05-17 23:53:50 +0200

    naive subscribe link

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import type { Component } from 'solid-js';
+import { Component } from 'solid-js';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+const SUBSCRIBE_HREF = "http://localhost:4000/subscribe"; //tmp, this will use some relative url
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div>
+      <a href={SUBSCRIBE_HREF} target="_blank">
+        subscribe
+      </a>
     </div>
   );
 };
